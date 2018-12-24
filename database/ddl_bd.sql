af32b1c6fbb22133aba74a22328caabfd14cf8da
Pedro <pedrlz.frn@gmail.com>
2018-12-24 17:49:52 -0200

    arrumando o ddl da db

--- database/ddl_bd.sql
+++ database/ddl_bd.sql
@@ -1,6 +1,11 @@
-@@ -1,47 +0,0 @@
+create table turma(
+  turma_id  serial not null,
+  nome varchar(6) unique not null,
+  primary key(turma_id)
+);
+
 CREATE TABLE pessoa(
-  pessoa_id integer serial not null,
+  pessoa_id serial not null,
   nome varchar(30) not null,
   sexo varchar(1) not null,
   email varchar not null,
@@ -8,24 +13,19 @@ CREATE TABLE pessoa(
 );
 
 create table telefone(
-  telefone_id integer serial not null,
+  telefone_id  serial not null,
   pessoa_id integer not null,
-  numero varchar(11) not null
+  numero varchar(11) not null,
   foreign key(pessoa_id) references pessoa(pessoa_id),
   primary key(pessoa_id,telefone_id)
-);
-
-create table turma(
-  turma_id integer serial not null,
-  nome varchar(6) unique not null,
-  primary key(turma_id)
 );
 
 create table aluno(
   aluno_id integer not null,
   matricula varchar(10) unique not null,
   turma_id integer not null,
-  foreign key(turma_id,aluno_id) references turma(turma_id),pessoa(pessoa_id)
+  foreign key(turma_id) references turma(turma_id),
+  foreign key(aluno_id) references pessoa(pessoa_id),
   primary key(aluno_id)
 );
 
@@ -34,12 +34,12 @@ create table gerente(
   login varchar not null,
   senha varchar not null,
   superUser boolean not null,
-  foreign key(gerente_id) references pessoa(pessoa_id)
+  foreign key(gerente_id) references pessoa(pessoa_id),
   primary key(gerente_id)
 );
 
 create table regES(
-  regES_id integer serial not null,
+  regES_id  serial not null,
   gerente_id integer not null,
   dataHoraEntrada timestamp not null,
   dataHoraSaida timestamp not null,
