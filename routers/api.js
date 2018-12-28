5fdcde1f944fc5ff6c5d62d540511dcdfb759573
Pedro <pedrlz.frn@gmail.com>
2018-12-28 10:49:45 -0200

    quick fix

--- routers/api.js
+++ routers/api.js
@@ -6,11 +6,15 @@ const alunosRouter = require('./api/alunos.js');
 const pessoasRouter = require('./api/pessoas.js');
 const telefonesRouter = require('./api/telefones.js');
 const gerentesRouter = require('./api/gerentes.js');
+const registrosRouter = require('./api/registros.js');
+const historicosRouter = require('./api/historicos.js');
 
 router.use('/turmas', turmasRouter);
 router.use('/pessoas', pessoasRouter);
 router.use('/alunos', alunosRouter);
 router.use('/telefones', telefonesRouter);
 router.use('/gerentes', gerentesRouter);
+router.use('/registros', registrosRouter);
+router.use('/historicos', historicosRouter);
 
 module.exports = router;
