45241373c87cb0fdda7158682b643a968a283eb6
Pedro <pedrlz.frn@gmail.com>
2019-01-26 13:51:25 -0200

    mudando static rota pro folder dist

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 const connString = process.argv[2];
 
 app.use(helmet());
-app.use(express.static('public'));
+app.use(express.static('dist'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
