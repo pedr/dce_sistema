df301c4eaff9a21a201e15be8f910a3407842355
Pedro <pedrlz.frn@gmail.com>
2019-01-27 21:15:51 -0200

    adicionar search pra /api/pedidos >> /pedidos/search?search="nome"

--- controller/pedidosController.js
+++ controller/pedidosController.js
@@ -21,6 +21,28 @@ controller.getAll = async (req, res) => {
   }
 };
 
+controller.getByName = async (req, res) => {
+  try {
+    const { search } = req.query;
+    const querySearch = `SELECT * FROM pedido pe
+    LEFT JOIN gerente g on g.gerenteid = pe.gerenteid
+    LEFT JOIN pessoa p on p.pessoaid = g.gerenteid
+    WHERE g.login = $1 OR
+    p.nome LIKE '%' || $1 || '%'`;
+    const result = await db.queryWithArgs(querySearch, [search]);
+    console.log(search);
+    const filtrado = result.map((a) => {
+      delete a.senha;
+      return a;
+    });
+
+    res.json(result);
+  } catch (err) {
+    console.error(err);
+    res.json(err);
+  }
+}
+
 controller.getOne = async (req, res) => {
   try {
     const { id } = req.params;
@@ -37,7 +59,7 @@ controller.getOne = async (req, res) => {
   }
 };
 
-async function addHistorico(gerenteId, alunoId = null, pedidoAtivo = true, copiaErrada, copiaCorreta) {
+async function addPedido(gerenteId, alunoId = null, pedidoAtivo = true, copiaErrada, copiaCorreta) {
   try {
     const queryStr = 'INSERT INTO pedido (gerenteId, alunoId, pedidoAtivo, copiaCorreta, copiaErrada) VALUES ($1, $2, $3, $4, $5) RETURNING *';
     const result = await db.queryWithArgs(queryStr,
@@ -71,7 +93,7 @@ controller.add = async (req, res) => {
       gerenteId, alunoId, pedidoAtivo, copiaErrada, copiaCorreta,
     } = data;
 
-    const result = await addHistorico(gerenteId, alunoId, pedidoAtivo, copiaErrada, copiaCorreta);
+    const result = await addPedido(gerenteId, alunoId, pedidoAtivo, copiaErrada, copiaCorreta);
 
     res.json(result);
   } catch (err) {
--- routers/api/pedidos.js
+++ routers/api/pedidos.js
@@ -4,6 +4,7 @@ const router = require('express').Router();
 const controller = require('../../controller/pedidosController.js');
 
 router.get('/', controller.getAll);
+router.get('/search', controller.getByName);
 router.get('/:id', controller.getOne);
 router.post('/', controller.add);
 
