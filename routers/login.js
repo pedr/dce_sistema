b7abc4bbcc9bfa11808d0dc1772dfcc9f27d711a
Pedro <pedrlz.frn@gmail.com>
2019-01-27 16:16:44 -0200

    esqueci de tirar uma rota desuclpa

--- routers/login.js
+++ routers/login.js
@@ -5,6 +5,5 @@ const controller = require('../controller/loginController.js');
 
 router.get('/', (req, res) => res.redirect('/login.html'));
 router.post('/in', controller.verify);
-router.post('/registrar', controller.registrar);
 
 module.exports = router;
