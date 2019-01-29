18f1237dcde82ec9ff22716f0dfdd021fa62a47a
Pedro <pedrlz.frn@gmail.com>
2019-01-29 12:00:51 -0200

    procurar por data, olhe no /routers/api/pedidos.js para ver o formato requerido

--- controller/pedidosController.js
+++ controller/pedidosController.js
@@ -57,6 +57,114 @@ controller.getByName = async (req, res) => {
   }
 }
 
+function checkIfDateIsValid(date) {
+  if (date.length !== 8) {
+    return false;
+  }
+  const year = date.slice(0, 4);
+  const month = date.slice(4, 6);
+  const day = date.slice(6, 8);
+
+  if (typeof +year !== 'number' || typeof +month !== 'number' || typeof +day !== 'number') {
+    return false;
+  }
+
+  if (month < 1 || month > 12) {
+    return false;
+  }
+
+  if (day < 1 || day > 31) {
+    return false;
+  }
+
+  return true;
+}
+
+function transformDateToPSQL(date) {
+  const year = `${date.slice(0, 4)}-`;
+  const month = `${date.slice(4, 6)}-`;
+  const newDate = `${year}${month}${date.slice(6, 8)}`;
+  return newDate;
+}
+
+function addCopias(pedidos) {
+  const result = {
+    copiasCorretasTotais: 0,
+    copiasErradasTotais: 0,
+    totalDePedidos: 0,
+  };
+  pedidos.forEach(obj => {
+    result.copiasCorretasTotais += obj.copiacorreta;
+    result.copiasErradasTotais += obj.copiaerrada;
+    result.totalDePedidos += 1;
+  });
+
+  return result;
+}
+
+controller.byDate = async (req, res) => {
+  try {
+    const { initial } = req.query;
+    let { final } = req.query;
+    const { format } = req.query;
+
+    const now = new Date();
+    const nowFormatted = (1900 + now.getYear())
+      + ((now.getMonth() + 1).toString().padStart(2, 0))
+      + now.getDate();
+
+    if (final === null) {
+      final = nowFormatted;
+    }
+
+    if (checkIfDateIsValid(initial) === false) {
+      res.json('invalid date format, should be "YYYYMMDD" ex. 20190101');
+      return;
+    }
+
+    const psqlInital = transformDateToPSQL(initial);
+    const psqlFinal = transformDateToPSQL(final);
+
+    const querySearch = `SELECT 
+      pe.pedidoid,
+      pe.alunoid,
+      pe.datahora,
+      pe.pedidoativo,
+      pe.copiacorreta,
+      pe.copiaerrada,
+      g.login,
+      g.superuser,
+      g.contaativa,
+      p.pessoaid,
+      p.nome,
+      p.sexo,
+      p.email
+    FROM pedido pe
+    LEFT JOIN gerente g ON g.gerenteid = pe.gerenteid
+    LEFT JOIN pessoa p ON g.gerenteid = p.pessoaid
+    WHERE pe.dataHora >= $1 AND pe.dataHora <= $2`;
+
+    const result = await db.queryWithArgs(querySearch, [psqlInital, psqlFinal]);
+
+    if (format === 'all') {
+      res.json(result);
+      return;
+    }
+
+    if (format === 'sum') {
+      const soma = addCopias(result);
+      soma.dataInicial = psqlInital;
+      soma.dataFinal = psqlFinal;
+      res.json(soma);
+      return;
+    }
+    res.json('algo deu errado');
+  } catch (err) {
+    console.error(err);
+    res.json(err);
+  }
+};
+
 async function addPedido(gerenteId, alunoId = null, pedidoAtivo = true, copiaErrada, copiaCorreta) {
   try {
     const queryStr = 'INSERT INTO pedido (gerenteId, alunoId, pedidoAtivo, copiaCorreta, copiaErrada) VALUES ($1, $2, $3, $4, $5) RETURNING *';
--- routers/api/pedidos.js
+++ routers/api/pedidos.js
@@ -2,9 +2,20 @@
 const router = require('express').Router();
 
 const controller = require('../../controller/pedidosController.js');
+/*
+ *
+ * /api/search?search=asdf
+ * /api/date?inital=YYYYMMDD?final=YYYYMMDD?format=[sum|all]
+ * final não necessaria, considera dia atual se não presente
+ * format pode ser SUM ou ALL,
+ *  sum soma os valores,
+ *    retornado copiacorreta, copiaerrada, datainital, datafinal
+ *  all retorna os pedidos inteiros
+*/
 
 router.get('/', controller.getAll);
 router.get('/search', controller.getByName);
+router.get('/date', controller.byDate);
 router.get('/:id', controller.getOne);
 router.post('/', controller.add);
 
