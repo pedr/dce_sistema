167fadc3bc054bb047e81255f48db2d85fc578d1
Pedro <pedrlz.frn@gmail.com>
2019-02-04 11:37:06 -0200

    numero nos erros

--- controller/authenticationController.js
+++ controller/authenticationController.js
@@ -7,24 +7,24 @@ controller.verifyJWT = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization) {
-    return res.send('algum erro');
+    return res.send('algum erro 1');
   }
 
   const parts = authorization.split(' ');
 
   if (!parts.length === 2) {
-    return res.send('algum erro');
+    return res.send('algum erro 2');
   }
 
   const [scheme, token] = parts;
 
   if (!/^Bearer$/i.test(scheme)) {
-    return res.send('algum erro');
+    return res.send('algum erro 3');
   }
 
   jwt.verify(token, process.env.SECRET, (err, decoded) => {
     if (err) {
-      return res.send('algum erro');
+      return res.send('algum erro 4');
     }
     req.userId = decoded.id;
     return next();
