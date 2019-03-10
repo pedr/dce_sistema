f862f6888281e034f625da502c9c3fda56b0c7be
marcelopbg <=>
2019-03-10 13:36:47 -0300

    vai disgraça

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+
 //const helmet = require('helmet');
 const apiRouter = require('./routers/api.js');
 const loginRouter = require('./routers/login.js');
@@ -9,7 +10,11 @@ const registrarRouter = require('./routers/registrar.js');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const connString = process.argv[2];
+var cors = require('cors')
 
+if (NODE_ENV !== 'production') {
+   app.use(cors())
+}
 //app.use(helmet());
 app.use(express.static('dist'));
 app.use(bodyParser.json());
