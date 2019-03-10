6efe32bf614220cf2817c6e83c55cd99b69c3304
marcelopbg <=>
2019-03-10 14:02:26 -0300

    tests and more tests

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const loginRouter = require('./routers/login.js');
 const registrarRouter = require('./routers/registrar.js');
 
 const app = express();
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 const PORT = process.env.PORT || 5000;
 const connString = process.argv[2];
 
@@ -16,20 +22,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 //ESTE TRECHO AUTORIZA REQUISICOES DE DIFERENTES END-POINTS (CROSS ORIGN REQUEST), DESTA FORMA CONSIGO CONSUMIR
 //A API NO MEU AMBIENTE DE DESENVOLVIMENTO, AO COLOCAR O APP EM PRODUÇÃO NUMA VERSÃO ESTÁVEL REMOVER TRECHO ABAIXO.
-app.use('/api', apiRouter);
-app.use('/login', loginRouter);
-app.use('/registrar', registrarRouter);
 
-app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 app.get('/', (req, res) => {
   res.send('Servidor funcionando, testando deploy automaticando qnd github atualiza');
 });
 
+app.use('/api', apiRouter);
+app.use('/login', loginRouter);
+app.use('/registrar', registrarRouter);
 
 app.listen(PORT, () => {
   console.log(`Acesse atraves de http://localhost:${PORT}`);
