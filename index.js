8b8434e6d2f941b618833a5f2767e39f9b56ed5a
marcelopbg <34853683+marcelopbg@users.noreply.github.com>
2019-03-10 12:24:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -28,12 +28,12 @@ app.use(function(req, res, next) {
 });
 app.get('/', (req, res) => {
   res.send('Servidor funcionando, testando deploy automaticando qnd github atualiza');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
-    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+   // res.setHeader('Access-Control-Allow-Origin', '*');
+    //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    //res.setHeader('Access-Control-Allow-Credentials', true); // If needed
 
-    res.send('cors problem fixed:)');
+    //res.send('cors problem fixed:)');
 });
 
 app.use('/api', apiRouter);
