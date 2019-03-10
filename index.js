12f6d08f9e4ec5ec66f0c20668525cf89d317928
marcelopbg <=>
2019-03-10 13:41:58 -0300

    remove cors

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-
 //const helmet = require('helmet');
 const apiRouter = require('./routers/api.js');
 const loginRouter = require('./routers/login.js');
@@ -10,11 +9,7 @@ const registrarRouter = require('./routers/registrar.js');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const connString = process.argv[2];
-var cors = require('cors')
 
-if (NODE_ENV !== 'production') {
-   app.use(cors())
-}
 //app.use(helmet());
 app.use(express.static('dist'));
 app.use(bodyParser.json());
@@ -35,7 +30,7 @@ app.get('/', (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.setHeader('Access-Control-Allow-Credentials', true);
+      res.setHeader('Access-Control-Allow-Credentials', true);
 
 });
 
